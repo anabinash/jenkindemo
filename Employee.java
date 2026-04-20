26c4513c961a627139a0d8303cc67906e781308c
anabinash <sonuabinash6370@gmail.com>
2026-04-20 22:18:38 +0530

    student commit

--- Employee.java
+++ Employee.java
@@ -2,5 +2,6 @@ public class Employee {
     public static void main(String[] args) {
         System.out.println("Employee class");
         System.out.println("Employee class demo");
+        System.out.println("Swechchha Singh is a good student!");
     }
 }
