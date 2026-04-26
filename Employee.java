a95abcd4875f30d7d6f5d2a0506a150dd89532ac
anabinash <sonuabinash6370@gmail.com>
2026-04-26 15:14:34 +0530

    add commit

--- Employee.java
+++ Employee.java
@@ -1,8 +1,6 @@
 public class Employee {
     public static void main(String[] args) {
-        System.out.println("Employee class");
-        System.out.println("Employee class demo");
-        System.out.println("Swechchha Singh is a good student!");
-        System.out.println("I love you");
+        System.out.println("CI / CD is a good technology ");
+       
     }
 }
